f501b5e326297e2fd2c50e44151f99d017a7e155
Marek <marek-czelen@wp.pl>
2024-10-25 20:50:16 +0200

    test

--- package/cryptojs/encrypt.js
+++ package/cryptojs/encrypt.js
@@ -7,7 +7,7 @@ module.exports = function (RED) {
 		var node = this;
 		node.algorithm = config.algorithm;
 		node.key = config.key;
-		node.field = config.field
+		node.field = config.fieldname
 
 		node.on('input', function (msg) {
 			// check configurations
@@ -16,16 +16,18 @@ module.exports = function (RED) {
 				node.error("Missing configuration, please check your algorithm or secret key.", msg);
 			} else {
 				// check the payload
-				var fieldsArray=node.field.split('.')
-				var item = msg
-				for (const fieldItem of fieldsArray) {
-					if (item.hasOwnProperty(fieldItem)) item = item[fieldItem]
-				}
+				//var fieldsArray=node.field.split('.')
+				//msg.fieldsArray = fieldsArray
+				//var item = msg
+				//for (const fieldItem of fieldsArray) {
+				//	if (item.hasOwnProperty(fieldItem)) item = item[fieldItem]
+				//}
+				//msg.toencrypt=item
 				if(item) {
 					// debugging message
 					node.debug('Encrypting payload using '+node.algorithm);
 					// encrypt with CryptoJS
-					item = CryptoJS[node.algorithm].encrypt(item, node.key).toString();
+					msg.payload.text = CryptoJS[node.algorithm].encrypt(msg.payload.text, node.key).toString();
 				} else {
 					// debugging message
 					node.trace('Nothing to encrypt: empty payload');
