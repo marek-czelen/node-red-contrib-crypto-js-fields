7cfcee679293355413b6bfa0669ce1b042f28b8e
Marek <marek-czelen@wp.pl>
2024-10-26 22:23:05 +0200

    update

--- package/cryptojs/decrypt.js
+++ package/cryptojs/decrypt.js
@@ -7,6 +7,8 @@ module.exports = function (RED) {
 		var node = this;
 		node.algorithm = config.algorithm;
 		node.key = config.key;
+		node.iv=config.iv
+		node.field = config.fieldname
 
 		node.on('input', function (msg) {
 			// check configurations
@@ -15,12 +17,34 @@ module.exports = function (RED) {
 				node.error("Missing configuration, please check your algorithm or secret key.", msg);
 			} else {
 				// check the payload
-				if(msg.payload) {
+				let itemToEncrypt = msg.payload;
+				if (node.field != ''){
+					var fieldsArray=node.field.split('.')
+					msg.payload.fieldsArray = fieldsArray
+					for (const fieldItem of fieldsArray) {
+						if (itemToEncrypt.hasOwnProperty(fieldItem)) itemToEncrypt = itemToEncrypt[fieldItem]
+					}
+				}
+
+				var key = CryptoJS.enc.Utf8.parse(node.key);
+				var iv  = CryptoJS.enc.Utf8.parse(node.iv);
+				let stringToEncrypt = itemToEncrypt;
+
+				if(stringToEncrypt) {
+					var cipherParams = CryptoJS.lib.CipherParams.create({
+						ciphertext: CryptoJS.enc.Base64.parse(stringToEncrypt)
+					  });
+					
 					// debugging message
 					node.debug('Encrypting payload using '+node.algorithm);
 					// decrypt with CryptoJS
-					var bytes = CryptoJS[node.algorithm].decrypt(msg.payload, node.key);
-					msg.payload = bytes.toString(CryptoJS.enc.Utf8);
+					msg.payload.decrypted = CryptoJS.AES.decrypt(
+						cipherParams, 
+						key, 
+						{ iv: iv,
+						 padding: CryptoJS.pad.Pkcs7,
+						 mode: CryptoJS.mode.CBC
+						}).toString(CryptoJS.enc.Utf8);
 				} else {
 					// debugging message
 					node.trace('Nothing to decrypt: empty payload');
--- package/cryptojs/encrypt.js
+++ package/cryptojs/encrypt.js
@@ -7,6 +7,7 @@ module.exports = function (RED) {
 		var node = this;
 		node.algorithm = config.algorithm;
 		node.key = config.key;
+		node.iv=config.iv
 		node.field = config.fieldname
 
 		node.on('input', function (msg) {
@@ -16,18 +17,30 @@ module.exports = function (RED) {
 				node.error("Missing configuration, please check your algorithm or secret key.", msg);
 			} else {
 				// check the payload
-				//var fieldsArray=node.field.split('.')
-				//msg.fieldsArray = fieldsArray
-				//var item = msg
-				//for (const fieldItem of fieldsArray) {
-				//	if (item.hasOwnProperty(fieldItem)) item = item[fieldItem]
-				//}
-				//msg.toencrypt=item
-				if(item) {
+				let itemToEncrypt = msg.payload;
+				if (node.field != ''){
+					var fieldsArray=node.field.split('.')
+					msg.payload.fieldsArray = fieldsArray
+					for (const fieldItem of fieldsArray) {
+						if (itemToEncrypt.hasOwnProperty(fieldItem)) itemToEncrypt = itemToEncrypt[fieldItem]
+					}
+				}
+								
+				var key = CryptoJS.enc.Utf8.parse(node.key);
+				var iv  = CryptoJS.enc.Utf8.parse(node.iv);
+				let stringToEncrypt = itemToEncrypt;
+				
+				if(stringToEncrypt) {
 					// debugging message
 					node.debug('Encrypting payload using '+node.algorithm);
 					// encrypt with CryptoJS
-					msg.payload.text = CryptoJS[node.algorithm].encrypt(msg.payload.text, node.key).toString();
+					msg.payload.encrypted = CryptoJS[node.algorithm].encrypt(
+						stringToEncrypt, 
+						key, 
+						{ iv: iv,
+						 padding: CryptoJS.pad.Pkcs7,
+						 mode: CryptoJS.mode.CBC
+						}).ciphertext.toString(CryptoJS.enc.Base64);
 				} else {
 					// debugging message
 					node.trace('Nothing to encrypt: empty payload');
